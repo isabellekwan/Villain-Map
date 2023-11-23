4fbd90e3d16dbff29b5236a0f6b35e2b3384bc24
Isabelle Kwan <bellekwan9@gmail.com>
2023-11-23 15:17:24 -0800

    Fixed map display error

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 
+import { icon, Marker } from 'leaflet';
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25,41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+
+Marker.prototype.options.icon = iconDefault;
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
 
-export class MapComponent {
+export class MapComponent implements OnInit {
   private map!: L.Map
 
   constructor(){}
