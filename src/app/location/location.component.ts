38342f8e7c0a5269246c69fdad07fc8d9811880c
Isabelle Kwan <bellekwan9@gmail.com>
2023-12-01 23:50:12 -0800

    Deleted unessecary code

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -42,12 +42,6 @@ export class LocationComponent implements OnInit {
 
   showMap() {
     this.map = L.map('mapid').setView([49.27, -123], 11);
-
-    const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> ',
-
-    }).addTo(this.map);
   }
 
   showClick() {
